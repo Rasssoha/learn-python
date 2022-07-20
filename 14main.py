07950860abcf2142dfffd263deea6e3134703e6e
Rassokha <100968371+Rasssoha@users.noreply.github.com>
2022-07-20 22:19:09 +0300

    Update 14main.py

--- 14main.py
+++ 14main.py
@@ -1,3 +1,13 @@
+from operator import index
+from xml.dom.minidom import Element
+
 def accum(s):
-    x = split(s)
-    print(s)
+    letter_list = list(s)
+    index_number = 0
+    for letter in letter_list:
+        index_number = index_number + 1
+        for i in range(index_number):
+            print(letter)
+
+accum("teueu")
+
